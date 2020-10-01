a2f94c37568e047e9b07a4baf03cd60461972e03
Alex Silva <alexbastiansilvafigueroa@gmail.com>
2020-10-01 16:13:13 -0300

    fix4

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,9 +3,11 @@ import "../assets/css/App.css";
 const Data = () => {
   return (
     <div className="data-container">
+    {
       <button type="button" id="btn">
         Compralo, Ya!
       </button>
+    }
     </div>
   );
 };
